af90d7e4a6168145535775a025a7b07bb111fa7a
Lzygmunt <lzygmunt0@gmail.com>
2020-02-23 16:23:43 +0100

    add favicons

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ roboData }) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${ roboData.id }?200x200`} alt="robots"/>
+      <img src={`https://robohash.org/${ roboData.id }?size=200x200`} alt="robots"/>
       <div>
         <h2>{ roboData.name }</h2>
         <p>{ roboData.email }</p>
--- src/components/header/SearchBox.js
+++ src/components/header/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchField, handleChange }) => {
   return (
     <div className="pa2">
       <input
+        aria-label="search robots"
         type="search"
         placeholder="search robots"
         className="pa3 ba b--green bg-lightest-blue"
